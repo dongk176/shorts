60e7ba0d453d650157302796ce9bfb8689ab4666
KIMDONGMIN <dmsthaalcls@gmail.com>
2026-07-20 02:26:40 +0900

    Add production shorts migrations

---
+++ supabase/migrations/202607170006_thepayone_recurring_payment_test_one_minute.sql
@@ -0,0 +1,13 @@
+begin;
+
+alter table shorts_mvp.payment_test_recurring_runs
+  drop constraint if exists payment_test_recurring_runs_interval_seconds_check;
+
+alter table shorts_mvp.payment_test_recurring_runs
+  alter column interval_seconds set default 60;
+
+alter table shorts_mvp.payment_test_recurring_runs
+  add constraint payment_test_recurring_runs_interval_seconds_check
+  check (interval_seconds in (60, 300));
+
+commit;
